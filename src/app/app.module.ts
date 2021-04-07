dfa31ecb63c72e0c140b28bee2dbb4d8eab24061
Marcos <marcoslsouzadf@gmail.com>
2021-04-07 01:14:33 -0300

    Criacao do componente calculadora

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalculadoraModule } from './calculadora'; // Foi usado o encurtamento de nomes "calculadora/index.ts"
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CalculadoraModule
   ],
   providers: [],
   bootstrap: [AppComponent]
